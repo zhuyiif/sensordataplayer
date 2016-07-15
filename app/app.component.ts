6064d947b0879585cef5a74bfe565e14087d9ce4
Zack Zhu <zack.zhu@daqri.com>
2016-07-15 16:03:57 -0700

    no message

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { BucketService }        from './bucket.service';
 import { JSONP_PROVIDERS }  from '@angular/http';
+
+declare var Plotly: any;
 export class Bucket {
   CreationDate: string;
   Name: string;
@@ -16,9 +18,12 @@ const MelonBuckets: Bucket[] = [
 
 @Component({
   selector: 'my-app',
- template: `
+  template: `
     <h1>{{title}}</h1>
     <h2>My buckets</h2>
+    <input type="file" (change)="fileChangeEvent($event)" placeholder="Upload file..." />
+    <button type="button" (click)="upload()">Upload</button>
+    <div id="myDiv" style="width: 1000px; height: 400px;"><!-- Plotly chart will be drawn inside this DIV --></div>
     <ul class="buckets">
       <li *ngFor="let buck of allbuckts"
         [class.selected]="buck === selectedHero"
@@ -84,25 +89,93 @@ const MelonBuckets: Bucket[] = [
       border-radius: 4px 0 0 4px;
     }
   `],
-   providers: [JSONP_PROVIDERS,BucketService]
+  providers: [JSONP_PROVIDERS, BucketService]
 })
 export class AppComponent {
 
   allbuckts = [];
-  constructor(private buckService: BucketService) { 
-  console.log('in construnctor ');
-  
-  buckService.getHeroes().subscribe(
-            res => {
-                this.allbuckts = res;
-            }, 
-            null, 
-            () => { console.log(this.allbuckts); });
-    
+  leftChannel: any[];
+  rightChannel: any[];
+  channelIndex: any[];
+  filesToUpload: Array<File>;
+  constructor(private buckService: BucketService) {
+    console.log('in construnctor ');
+    this.filesToUpload = [];
+    this.leftChannel = [];
+    this.rightChannel = [];
+    this.channelIndex = [];
+    console.log(this.leftChannel);
+    buckService.getHeroes().subscribe(
+      res => {
+        this.allbuckts = res;
+      },
+      null,
+      () => { console.log(this.allbuckts); });
+
   }
+
   title = 'Tour of Buckets';
   buckets = MelonBuckets;
   selectedHero: Bucket;
-  onSelect(sbucket: Bucket) 
-  { this.selectedHero = sbucket; }
+  onSelect(sbucket: Bucket) {
+    this.selectedHero = sbucket;
+  }
+
+  upload() {
+    this.makeFileRequest("", [], this.filesToUpload);
+
+
+  }
+
+  fileChangeEvent(fileInput: any) {
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+  }
+  makeFileRequest(url: string, params: Array<string>, files: Array<File>) {
+    var formData: any = new FormData();
+    for (var i = 0; i < files.length; i++) {
+      formData.append("uploads[]", files[i], files[i].name);
+      console.log(files[i]);
+    }
+
+    if (files.length > 0) {
+      var myReader: FileReader = new FileReader();
+
+      var self = this;
+      myReader.onloadend = function (e) {
+        var lines = myReader.result.split("\n");
+        lines.forEach(function (element, index, array) {
+          var lineitem: any[] = element.split(/(\s+)/);
+          self.leftChannel.push(lineitem[4]);
+          self.rightChannel.push(lineitem[6]);
+          self.channelIndex.push(lineitem[0]);
+        }, self);
+
+
+        var trace1 = {
+          x: self.channelIndex.slice(0, 500),
+          y: self.leftChannel.slice(0, 500),
+          type: 'scatter'
+        };
+
+        var trace2 = {
+          x: self.channelIndex.slice(0, 500),
+          y: self.rightChannel.slice(0, 500),
+          type: 'scatter'
+        };
+
+        var data = [trace1, trace2];
+
+        Plotly.newPlot('myDiv', data);
+
+
+      }
+
+
+
+
+      myReader.readAsText(files[0]);
+
+    }
+
+  }
 }
